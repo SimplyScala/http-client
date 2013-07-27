2b91fbfff7b705c4f44c0d4a5f50c981c51e6409
ubourdon <bourdon.ugo@gmail.com>
2013-07-27 17:56:48 +0200

    add: first AsyncHttpClient.post() method

--- src/main/scala/com/github/simplyscala/http/client/AsyncHttpClient.scala
+++ src/main/scala/com/github/simplyscala/http/client/AsyncHttpClient.scala
@@ -9,14 +9,29 @@ class AsyncHttpClient {
     def get(url: String): Future[Response] = {
         val promise = Promise[Response]()
 
-        javaClient.prepareGet(url).execute(new AsyncCompletionHandler[Response]{
-            def onCompleted(response: Response): Response = {
-                promise.success(response)
-                response
-            }
-        })
+        executeGetRequest(url, promise)
 
         promise.future
+    }
+
+    def post(url: String, params: Map[String,String] = Map()): Future[Response] = {
+        val promise = Promise[Response]()
+
+        executePostRequest(url, promise)
+
+        promise.future
+    }
+
+    private def executeGetRequest(url: String, promise: Promise[Response]) {
+        javaClient.prepareGet(url).execute(new AsyncCompletionHandler[Response] {
+            def onCompleted(response: Response): Response = { promise.success(response); response }
+        })
+    }
+
+    private def executePostRequest(url: String, promise: Promise[Response]) {
+        javaClient.preparePost(url).execute(new AsyncCompletionHandler[Response] {
+            def onCompleted(response: Response): Response = { promise.success(response); response }
+        })
     }
 }
 
--- src/test/scala/com/github/simplyscala/http/client/AsyncHttpClientTest.scala
+++ src/test/scala/com/github/simplyscala/http/client/AsyncHttpClientTest.scala
@@ -3,7 +3,7 @@ package com.github.simplyscala.http.client
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
 import fr.simply.fixture.StubServerFixture
-import fr.simply.{StaticServerResponse, GET}
+import fr.simply.{POST, StaticServerResponse, GET}
 import fr.simply.util.Text_Plain
 import concurrent.{Await, Future}
 import com.ning.http.client.{ListenableFuture, Response}
@@ -11,7 +11,7 @@ import concurrent.duration.Duration
 
 class AsyncHttpClientTest extends FunSuite with ShouldMatchers with StubServerFixture {
 
-    test("should return response") {
+    test("[GET] request should return response") {
         val route = GET (
             path = "*",
             response = StaticServerResponse(Text_Plain, "yo", 200)
@@ -19,6 +19,18 @@ class AsyncHttpClientTest extends FunSuite with ShouldMatchers with StubServerFi
 
         withStubServerFixture(8080, route) { server =>
             val response: Future[Response] = new AsyncHttpClient().get(s"http://localhost:${server.portInUse}")
+            Await.result(response, Duration.Inf).getStatusCode should be (200)
+        }
+    }
+
+    test("[POST] request should return response") {
+        val route = POST (
+            path = "*",
+            response = StaticServerResponse(Text_Plain, "yo", 200)
+        )
+
+        withStubServerFixture(8080, route) { server =>
+            val response: Future[Response] = new AsyncHttpClient().post(s"http://localhost:${server.portInUse}")
             Await.result(response, Duration.Inf).getStatusCode should be (200)
         }
     }
